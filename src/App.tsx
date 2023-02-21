83d44fc24d52a599f01c4ac1fc361f1925d0a157
Hernan Plaza <hernan.plazabs@gmail.com>
2023-02-21 13:08:41 -0500

    [CLEAR] App logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
-import { StepOneForm } from "./pages/components/StepOneForm";
 import { MultiStepForm } from "./pages/MultiStepForm";
 
 function App() {
-  const steps = [
-    {
-      title: "Personal info",
-      description: "Please provide your name, email address and phone number",
-      component: <StepOneForm />,
-      isComplete: true,
-    },
-  ];
-
   return <MultiStepForm />;
 }
 
